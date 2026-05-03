9f8d61b73b7bb650604d66317d175b66021b9fed
Yakov <belkovyakov@gmail.com>
2026-05-03 22:31:42 +0200

    glfw

--- td_project_preparation/src/main.cpp
+++ td_project_preparation/src/main.cpp
@@ -1,16 +1,17 @@
 #include <glad/gl.h>
-#include <SDL.h>
+#include <GLFW/glfw3.h>
 #include <cmath>
 #include <cstdio>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <filesystem>
 #include "mat4.h"
 
 static std::string read_file(const std::string& path) {
     std::ifstream f(path);
     if (!f) {
-        SDL_Log("cannot open %s", path.c_str());
+        std::fprintf(stderr, "cannot open %s\n", path.c_str());
         return {};
     }
     std::stringstream ss;
@@ -27,7 +28,7 @@ static GLuint compile_shader(GLenum type, const char* src) {
     if (!ok) {
         char log[1024];
         glGetShaderInfoLog(s, 1024, nullptr, log);
-        SDL_Log("shader error: %s", log);
+        std::fprintf(stderr, "shader error: %s\n", log);
     }
     return s;
 }
@@ -46,7 +47,7 @@ static GLuint make_program(const std::string& vertPath, const std::string& fragP
     if (!ok) {
         char log[1024];
         glGetProgramInfoLog(p, 1024, nullptr, log);
-        SDL_Log("link error: %s", log);
+        std::fprintf(stderr, "link error: %s\n", log);
     }
     glDeleteShader(vs);
     glDeleteShader(fs);
@@ -55,42 +56,41 @@ static GLuint make_program(const std::string& vertPath, const std::string& fragP
 
 // cube 36 vertices position + normale
 static const float CUBE[] = {
-    // back face (-Z)
     -0.5f,-0.5f,-0.5f,  0,0,-1,
      0.5f, 0.5f,-0.5f,  0,0,-1,
      0.5f,-0.5f,-0.5f,  0,0,-1,
      0.5f, 0.5f,-0.5f,  0,0,-1,
     -0.5f,-0.5f,-0.5f,  0,0,-1,
     -0.5f, 0.5f,-0.5f,  0,0,-1,
-    // front face (+Z)
+
     -0.5f,-0.5f, 0.5f,  0,0,1,
      0.5f,-0.5f, 0.5f,  0,0,1,
      0.5f, 0.5f, 0.5f,  0,0,1,
      0.5f, 0.5f, 0.5f,  0,0,1,
     -0.5f, 0.5f, 0.5f,  0,0,1,
     -0.5f,-0.5f, 0.5f,  0,0,1,
-    // left face (-X)
+
     -0.5f, 0.5f, 0.5f, -1,0,0,
     -0.5f, 0.5f,-0.5f, -1,0,0,
     -0.5f,-0.5f,-0.5f, -1,0,0,
     -0.5f,-0.5f,-0.5f, -1,0,0,
     -0.5f,-0.5f, 0.5f, -1,0,0,
     -0.5f, 0.5f, 0.5f, -1,0,0,
-    // right face (+X)
+
      0.5f, 0.5f, 0.5f,  1,0,0,
      0.5f,-0.5f,-0.5f,  1,0,0,
      0.5f, 0.5f,-0.5f,  1,0,0,
      0.5f,-0.5f,-0.5f,  1,0,0,
      0.5f, 0.5f, 0.5f,  1,0,0,
      0.5f,-0.5f, 0.5f,  1,0,0,
-    // bottom (-Y)
+
     -0.5f,-0.5f,-0.5f,  0,-1,0,
      0.5f,-0.5f,-0.5f,  0,-1,0,
      0.5f,-0.5f, 0.5f,  0,-1,0,
      0.5f,-0.5f, 0.5f,  0,-1,0,
     -0.5f,-0.5f, 0.5f,  0,-1,0,
     -0.5f,-0.5f,-0.5f,  0,-1,0,
-    // top (+Y)
+
     -0.5f, 0.5f,-0.5f,  0,1,0,
      0.5f, 0.5f, 0.5f,  0,1,0,
      0.5f, 0.5f,-0.5f,  0,1,0,
@@ -101,9 +101,18 @@ static const float CUBE[] = {
 
 struct OrbitCam {
     float radius = 5.0f;
-    float phi = 0.0f;     // azimut
-    float theta = 0.3f;   // elevation
+    float phi = 0.0f;
+    float theta = 0.3f;
 };
+
+struct AppState {
+    OrbitCam cam;
+    bool dragging = false;
+    double lastX = 0, lastY = 0;
+    int W = 1024, H = 768;
+};
+
+static const float PI = 3.14159265358979f;
 
 static Vec3 orbit_position(const OrbitCam& c, Vec3 target) {
     float x = c.radius * std::cos(c.theta) * std::cos(c.phi);
@@ -112,43 +121,92 @@ static Vec3 orbit_position(const OrbitCam& c, Vec3 target) {
     return { target.x + x, target.y + y, target.z + z };
 }
 
+static void on_resize(GLFWwindow* w, int width, int height) {
+    auto* s = (AppState*)glfwGetWindowUserPointer(w);
+    s->W = width; s->H = height;
+    glViewport(0, 0, width, height);
+}
+
+static void on_mouse_button(GLFWwindow* w, int button, int action, int mods) {
+    auto* s = (AppState*)glfwGetWindowUserPointer(w);
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+        if (action == GLFW_PRESS) {
+            s->dragging = true;
+            glfwGetCursorPos(w, &s->lastX, &s->lastY);
+        } else if (action == GLFW_RELEASE) {
+            s->dragging = false;
+        }
+    }
+}
+
+static void on_cursor_pos(GLFWwindow* w, double x, double y) {
+    auto* s = (AppState*)glfwGetWindowUserPointer(w);
+    if (!s->dragging) return;
+    double dx = x - s->lastX;
+    double dy = y - s->lastY;
+    s->lastX = x; s->lastY = y;
+    s->cam.phi   += (float)dx * 0.005f;
+    s->cam.theta += (float)dy * 0.005f;
+    if (s->cam.phi > PI) s->cam.phi -= 2*PI;
+    if (s->cam.phi < -PI) s->cam.phi += 2*PI;
+    float lim = PI*0.5f - 0.01f;
+    if (s->cam.theta > lim) s->cam.theta = lim;
+    if (s->cam.theta < -lim) s->cam.theta = -lim;
+}
+
+static void on_scroll(GLFWwindow* w, double xoff, double yoff) {
+    auto* s = (AppState*)glfwGetWindowUserPointer(w);
+    s->cam.radius -= (float)yoff * 0.5f;
+    if (s->cam.radius < 1.0f) s->cam.radius = 1.0f;
+    if (s->cam.radius > 50.0f) s->cam.radius = 50.0f;
+}
+
+static void on_key(GLFWwindow* w, int key, int sc, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(w, GLFW_TRUE);
+    }
+}
+
 int main(int argc, char** argv) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("SDL_Init: %s", SDL_GetError());
+    if (!glfwInit()) {
+        std::fprintf(stderr, "glfwInit failed\n");
         return 1;
     }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
-    int W = 1024, H = 768;
-    SDL_Window* win = SDL_CreateWindow(
-        "Camera orbitale",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        W, H,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-    );
-    if (!win) { SDL_Log("CreateWindow: %s", SDL_GetError()); return 1; }
+    AppState state;
+    GLFWwindow* win = glfwCreateWindow(state.W, state.H, "Camera orbitale", nullptr, nullptr);
+    if (!win) {
+        std::fprintf(stderr, "CreateWindow failed\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwSetWindowUserPointer(win, &state);
+    glfwMakeContextCurrent(win);
+    glfwSwapInterval(1);
 
-    SDL_GLContext ctx = SDL_GL_CreateContext(win);
-    if (!ctx) { SDL_Log("GL_CreateContext: %s", SDL_GetError()); return 1; }
-    SDL_GL_SetSwapInterval(1);
+    glfwSetFramebufferSizeCallback(win, on_resize);
+    glfwSetMouseButtonCallback(win, on_mouse_button);
+    glfwSetCursorPosCallback(win, on_cursor_pos);
+    glfwSetScrollCallback(win, on_scroll);
+    glfwSetKeyCallback(win, on_key);
 
-    if (!gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress)) {
-        SDL_Log("gladLoadGL failed");
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+        std::fprintf(stderr, "gladLoadGL failed\n");
         return 1;
     }
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-    char* base = SDL_GetBasePath();
-    std::string baseDir = base ? std::string(base) : std::string();
-    SDL_free(base);
-    GLuint prog = make_program(baseDir + "shaders/basic.vert", baseDir + "shaders/basic.frag");
+    std::filesystem::path exeDir = std::filesystem::path(argv[0]).parent_path();
+    std::string vertPath = (exeDir / "shaders" / "basic.vert").string();
+    std::string fragPath = (exeDir / "shaders" / "basic.frag").string();
+    GLuint prog = make_program(vertPath, fragPath);
 
     GLuint vao, vbo;
     glGenVertexArrays(1, &vao);
@@ -166,58 +224,25 @@ int main(int argc, char** argv) {
     GLint locProj  = glGetUniformLocation(prog, "uProj");
     GLint locColor = glGetUniformLocation(prog, "uColor");
 
-    OrbitCam cam;
     Vec3 target = { 0, 0, 0 };
-    bool dragging = false;
 
-    const float PI = 3.14f;
-
-    bool running = true;
-    Uint32 last = SDL_GetTicks();
+    double last = glfwGetTime();
     float t = 0.0f;
 
-    while (running) {
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) running = false;
-            else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
-            else if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_RESIZED) {
-                W = e.window.data1; H = e.window.data2;
-                glViewport(0, 0, W, H);
-            }
-            else if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) {
-                dragging = true;
-            }
-            else if (e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_LEFT) {
-                dragging = false;
-            }
-            else if (e.type == SDL_MOUSEMOTION && dragging) {
-                cam.phi   += e.motion.xrel * 0.005f;
-                cam.theta += e.motion.yrel * 0.005f;
-                if (cam.phi > PI) cam.phi -= 2*PI;
-                if (cam.phi < -PI) cam.phi += 2*PI;
-                float lim = PI*0.5f - 0.01f;
-                if (cam.theta > lim) cam.theta = lim;
-                if (cam.theta < -lim) cam.theta = -lim;
-            }
-            else if (e.type == SDL_MOUSEWHEEL) {
-                cam.radius -= e.wheel.y * 0.5f;
-                if (cam.radius < 1.0f) cam.radius = 1.0f;
-                if (cam.radius > 50.0f) cam.radius = 50.0f;
-            }
-        }
+    while (!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
 
-        Uint32 now = SDL_GetTicks();
-        float dt = (now - last) / 1000.0f;
+        double now = glfwGetTime();
+        float dt = (float)(now - last);
         last = now;
         t += dt;
 
         glClearColor(0.1f, 0.12f, 0.15f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        Vec3 camPos = orbit_position(cam, target);
+        Vec3 camPos = orbit_position(state.cam, target);
         Mat4 view = mat4_lookAt(camPos, target, { 0, 1, 0 });
-        Mat4 proj = mat4_perspective(60.0f * PI / 180.0f, (float)W / (float)H, 0.1f, 100.0f);
+        Mat4 proj = mat4_perspective(60.0f * PI / 180.0f, (float)state.W / (float)state.H, 0.1f, 100.0f);
 
         glUseProgram(prog);
         glUniformMatrix4fv(locView, 1, GL_FALSE, view.m);
@@ -225,7 +250,7 @@ int main(int argc, char** argv) {
 
         glBindVertexArray(vao);
 
-        // cube1 centrre tourne
+        // cube 1
         {
             Mat4 T = mat4_translation(0, 0, 0);
             Mat4 R = mat4_mul(mat4_rotY(t * 0.7f), mat4_rotX(t * 0.3f));
@@ -236,7 +261,7 @@ int main(int argc, char** argv) {
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        // cube2 sur le cote plus petit
+        // cube 2
         {
             Mat4 T = mat4_translation(2.0f, 0.5f, -1.0f);
             Mat4 R = mat4_rotY(t * -0.5f);
@@ -257,11 +282,10 @@ int main(int argc, char** argv) {
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        SDL_GL_SwapWindow(win);
+        glfwSwapBuffers(win);
     }
 
-    SDL_GL_DeleteContext(ctx);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+    glfwDestroyWindow(win);
+    glfwTerminate();
     return 0;
 }
